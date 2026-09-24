f847674d7d8f0ab7e60cc0b74ee9a9e02d320ba4
Jake Bybee <jake.bybee@bybeemail.com>
2026-09-23 20:44:53 -0600

    add mutex

--- internal/peers/peers.go
+++ internal/peers/peers.go
@@ -3,9 +3,11 @@ package peers
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 type PeersTable struct {
+	mu    sync.RWMutex
 	Peers map[string]*Peer
 }
 type Peer struct {
@@ -35,12 +37,17 @@ func initPeersTable() *PeersTable {
 }
 
 func RegisterPeer(peer Peer) *Peer {
+	peersTable.mu.Lock()
+	defer peersTable.mu.Unlock()
 	peersTable.Peers[peer.Id] = &peer
-	return peersTable.Peers[peer.Id]
+	registeredPeer := *peersTable.Peers[peer.Id]
+	return &registeredPeer
 }
 
 func GetAllPeers() []Peer {
 	peers := []Peer{}
+	peersTable.mu.RLock()
+	defer peersTable.mu.RUnlock()
 	for _, peer := range peersTable.Peers {
 		peers = append(peers, *peer)
 	}
@@ -48,17 +55,23 @@ func GetAllPeers() []Peer {
 }
 
 func GetPeerById(peerId string) *Peer {
+	peersTable.mu.RLock()
+	defer peersTable.mu.RUnlock()
 	if peer, exists := peersTable.Peers[peerId]; exists {
-		return peer
+		peerSnapshot := *peer
+		return &peerSnapshot
 	}
 	return nil
 }
 
 func UpdatePeerStatus(peerId string, status string) *Peer {
-	peer := GetPeerById(peerId)
+	peersTable.mu.Lock()
+	defer peersTable.mu.Unlock()
+	peer := peersTable.Peers[peerId]
 	if peer != nil {
 		peer.Status = status
-		return peer
+		peerSnapshot := *peer
+		return &peerSnapshot
 	}
 	return nil
 }
