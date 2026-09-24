951c0720676de7a790b41316667b3d2c48da2afd
Jake Bybee <jake.bybee@bybeemail.com>
2026-09-24 17:50:08 -0600

    register eroutes

---
+++ internal/server/routes.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (s *Server) RegisterRoutes() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		s.HealthHandler(w, r)
+	})
+
+	http.HandleFunc("/registerPeer", func(w http.ResponseWriter, r *http.Request) {
+		s.RegisterPeerHandler(w, r)
+	})
+
+	http.HandleFunc("/relayPeers", func(w http.ResponseWriter, r *http.Request) {
+		s.RelayPeersHandler(w, r)
+	})
+
+	http.HandleFunc("/registerPeersReceived", func(w http.ResponseWriter, r *http.Request) {
+		s.RegisterPeersReceivedHandler(w, r)
+	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		data, err := s.immichStore.BulkDownload(
+			[]string{
+
+				"00012798-03fa-478a-a79e-e54641afdfd6",
+				"000212d9-2eb7-41e9-907b-dc819d139e9b",
+				"0002ef04-6445-4440-9c65-61fdc18c352d",
+			},
+		)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to get file sizes data: %v", err), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "File sizes data: %v", data)
+	})
+}
